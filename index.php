f747f4ba706a9b506e134609a3a0c7d375ba0b59
pantera17 <josenaranjo2004.jn@gmail.com>
2021-12-10 16:29:56 -0500

    asignacion y operadores

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+//asignacion
+$num = 9;
+$lang = [
+    'es' => 'español',
+    'en' => 'ingles'
+];
+
+
+// Aritmetica
+echo "Suma 2 + 2 ". ((int) 2 + (int) 2);
+echo "Resta 2 - 2 ". ((int) 2 - (int) 2);
+echo "Multiplicar 2 * 2 ". 2 * 2;
+echo "dividir 2 / 2 ". 2 / 2;
+echo "Modulo 2 % 2 ". 2 % 2;
+echo "Exponencial 2 ** 2 ". 2 ** 2;
